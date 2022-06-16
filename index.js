6fa0a189e4120a54073eac112260699603c976ed
Brian Wangora <brianwangora@gmail.com>
2022-06-16 22:12:12 +0300

    commit tree 6c937860499e987c98c75753185e37ac3c2de65e

--- index.js
+++ index.js
@@ -1,13 +1,71 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  // create an object to keep track of all the numbers we've seen
+  const seenNumbers = {};
+  // iterate over the array of numbers
+  for (const number of array) {
+    // for the current number, identify a complementary number that adds to our target
+    // (for example: if our number is 2, and the target is 5, the complementary number is 3)
+    const complement = target - number;
+    // check if any of the keys in our object is the complement to the current number
+    // if so, return true
+    if (seenNumbers[complement]) return true;
+    // save the current number as the key on our object so we can check it later against other numbers
+    seenNumbers[number] = true;
+  }
+  // if we reach the end of the array, return false
+  return false;
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  Time complexity
+  function hasTargetSum(array, target) {
+  // 1 step
+  const seenNumbers = {};
+  for (const number of array) {
+    // n steps
+    const complement = target - number;
+    // n steps
+    if (seenNumbers[complement]) return true;
+    // n steps
+    seenNumbers[number] = true;
+  }
+  // 1 step
+  return false;
+}
+O(3n + 2) which is simplieified to O(n).
+massive improvement from O(n*n)
 */
 
 /* 
   Add your pseudocode here
+  create an object to keep track of all the numbers we've seen
+  iterate over the array of numbers
+  for the current number, identify a complementary number that adds to our target
+  (for example: if our number is 2, and the target is 5, the complementary number is 3)
+  check if any of the keys in our object is the complement to the current number
+  if so, return true
+  save the current number as the key on our object so we can check it later against other numbers
+  if we reach the end of the array, return false
+*/
+
+/*
+A Set is a very useful data structure to know for solving algorithm problems — any time you need to keep track of a list of unique values, it's a good data structure to consider!
+In our case, it's a bit more straightforward to use than an object, since an object is really meant for storing key-value pairs, and all we really care about are the keys.
+Here's how we can use a Set:
+
+function hasTargetSum(array, target) {
+  const seenNumbers = new Set(); // initialize an empty Set
+  for (const number of array) {
+    const complement = target - number;
+
+    // .has returns true if the Set includes the complement
+    if (seenNumbers.has(complement)) return true;
+
+    // .add adds the number to the Set
+    seenNumbers.add(number);
+  }
+  return false;
+}
 */
 
 /*
